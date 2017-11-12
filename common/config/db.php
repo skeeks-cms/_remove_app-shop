195013bb7acd20ed3d653e70dc4452ae451339e4
Саша <semenov@skeeks.com>
2017-11-12 16:19:08 +0300

    SkeekS CMS 5.0.0-alpha

--- common/config/db.php
+++ common/config/db.php
@@ -2,16 +2,20 @@
 /**
  * @author Semenov Alexander <semenov@skeeks.com>
  * @link http://skeeks.com/
- * @copyright 2010 SkeekS (ÑêèêÑ)
+ * @copyright 2010 SkeekS (ï¿½ï¿½ï¿½ï¿½ï¿½)
  * @date 18.09.2015
  */
 return
 [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=shop',
-    'username' => 'shop',
-    'password' => 'jW4mKYSVRVZPGyNn',
-    'charset' => 'utf8',
-    'enableSchemaCache' => true,
-    'schemaCacheDuration' => 3600,
+    'components'    => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=shop',
+            'username' => 'shop',
+            'password' => 'jW4mKYSVRVZPGyNn',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 3600,
+        ]
+    ]
 ];
